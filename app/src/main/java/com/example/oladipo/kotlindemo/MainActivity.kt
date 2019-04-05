32aa3775660df803be47aff7f8e22f48e3be37f5
dfrank7 <francisoladipo5@gmail.com>
2019-04-05 02:57:31 +0100

    newWordACt, styles

--- app/src/main/java/com/example/oladipo/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/oladipo/kotlindemo/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.example.oladipo.kotlindemo
 
+import android.app.Activity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.widget.Toast
+import com.example.oladipo.kotlindemo.adapter.WordListAdapter
+import com.example.oladipo.kotlindemo.entity.Word
+import com.example.oladipo.kotlindemo.viewmodel.WordViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var wordViewModel: WordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
+
+    }
+    companion object {
+        const val newWordActivityRequestCode = 1
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.let {
+                val word = Word(it.getStringExtra(NewWordActivity.EXTRA_REPLY))
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/oladipo/kotlindemo/database/WordRoomDatabase.kt
+++ app/src/main/java/com/example/oladipo/kotlindemo/database/WordRoomDatabase.kt
@@ -1,11 +1,16 @@
 package com.example.oladipo.kotlindemo.database
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
 import com.example.oladipo.kotlindemo.dao.WordDao
 import com.example.oladipo.kotlindemo.entity.Word
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.IO
+import kotlinx.coroutines.experimental.launch
 
 @Database(entities = [Word::class], version = 1)
 abstract class WordRoomDatabase: RoomDatabase() {
@@ -15,16 +20,34 @@ abstract class WordRoomDatabase: RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context,
+                        scope: CoroutineScope
+        ): WordRoomDatabase {
+
             return INSTANCE ?: synchronized(this) {
                 // Create database here
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "Word_database"
-                ).build()
+                ).addCallback(WordDatabaseCallback(scope))
+                    .build()
                     INSTANCE = instance
                 instance
+            }
+        }
+
+        private class WordDatabaseCallback(
+            private val scope: CoroutineScope
+        ) : RoomDatabase.Callback() {
+
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                INSTANCE?.let { database ->
+                    scope.launch(Dispatchers.IO) {
+                        //populateDatabase(database.wordDao())
+                    }
+                }
             }
         }
     }
